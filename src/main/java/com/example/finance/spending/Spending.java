9869d4cb31584e285897f1236704e483f55664ad
tolobayo <tolobayo1@gmail.com>
2024-10-12 18:59:41 -0400

    complete spending entity

--- src/main/java/com/example/finance/spending/Spending.java
+++ src/main/java/com/example/finance/spending/Spending.java
@@ -2,62 +2,143 @@ package com.example.finance.spending;
 
 import java.time.LocalDate;
 
+import com.example.finance.catagory.Catagory;
+import com.example.finance.user.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity(name = "Spending")
+@Table(name = "spending")
 public class Spending {
+    @Id
+    @SequenceGenerator(name = "spending_item_sequence",
+    sequenceName = "spending_item_sequence",
+    allocationSize = 1)
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "spending_item_sequence"
+    )
+    @Column(
+        name = "id",
+        updatable = false
+    )
     private Long id;
-    private Long user_id;
-    private Long catagory_id;
-    private Double amount_spent;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false )
+    private User user;
+
+    //Note: each spending item can only have one catagory
+    //TODO: create default value if catagory not set
+    @ManyToOne
+    @JoinColumn(name = "catagory_id", nullable = false)
+    private Catagory catagory;
+
+    @Column(
+        name = "label",
+        nullable = false,
+        columnDefinition = "TEXT"
+    )
+    private String label;
+
+    @Column(
+        name = "amount_spent",
+        nullable = false,
+        columnDefinition = "NUMERIC(10, 2)"
+    )
+    private Double amountSpent;
+
+    @Column(
+        name = "date",
+        nullable = false,
+        columnDefinition = "DATE"
+    )
     private LocalDate date;
+    
     public Spending() {
     }
-    public Spending(Long id, Long user_id, Long catagory_id, Double amount_spent, LocalDate date) {
+
+    public Spending(Long id, User user, Catagory catagory, String label, Double amountSpent, LocalDate date) {
         this.id = id;
-        this.user_id = user_id;
-        this.catagory_id = catagory_id;
-        this.amount_spent = amount_spent;
+        this.user = user;
+        this.catagory = catagory;
+        this.label = label;
+        this.amountSpent = amountSpent;
         this.date = date;
     }
-    public Spending(Long user_id, Long catagory_id, Double amount_spent, LocalDate date) {
-        this.user_id = user_id;
-        this.catagory_id = catagory_id;
-        this.amount_spent = amount_spent;
+
+    public Spending(User user, Catagory catagory, String label, Double amountSpent, LocalDate date) {
+        this.user = user;
+        this.catagory = catagory;
+        this.label = label;
+        this.amountSpent = amountSpent;
         this.date = date;
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
-    public Long getUser_id() {
-        return user_id;
+
+    public User getUser() {
+        return user;
     }
-    public void setUser_id(Long user_id) {
-        this.user_id = user_id;
+
+    public void setUser(User user) {
+        this.user = user;
     }
-    public Long getCatagory_id() {
-        return catagory_id;
+
+    public Catagory getCatagory() {
+        return catagory;
     }
-    public void setCatagory_id(Long catagory_id) {
-        this.catagory_id = catagory_id;
+
+    public void setCatagory(Catagory catagory) {
+        this.catagory = catagory;
     }
-    public Double getAmount_spent() {
-        return amount_spent;
+
+    public String getLabel() {
+        return label;
     }
-    public void setAmount_spent(Double amount_spent) {
-        this.amount_spent = amount_spent;
+
+    public void setLabel(String label) {
+        this.label = label;
     }
+
+    public Double getAmountSpent() {
+        return amountSpent;
+    }
+
+    public void setAmountSpent(Double amountSpent) {
+        this.amountSpent = amountSpent;
+    }
+
     public LocalDate getDate() {
         return date;
     }
+
     public void setDate(LocalDate date) {
         this.date = date;
     }
+
     @Override
     public String toString() {
-        return "Spending [id=" + id + ", user_id=" + user_id + ", catagory_id=" + catagory_id + ", amount_spent="
-                + amount_spent + ", date=" + date + "]";
+        return "Spending [id=" + id + ", user=" + user + ", catagory=" + catagory + ", label=" + label
+                + ", amountSpent=" + amountSpent + ", date=" + date + "]";
     }
+
+    
+    
 
     
 }
