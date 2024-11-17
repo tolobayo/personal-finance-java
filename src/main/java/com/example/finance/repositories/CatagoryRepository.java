7ab90f56aa3e6aeaa8138b8762d845fa262e9d23
tolobayo <tolobayo1@gmail.com>
2024-11-17 14:50:22 -0500

    remove unused query import

--- src/main/java/com/example/finance/repositories/CatagoryRepository.java
+++ src/main/java/com/example/finance/repositories/CatagoryRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.example.finance.entities.Catagory;
 import com.example.finance.entities.User;
